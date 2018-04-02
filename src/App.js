f4c27c3a84bc0398bebeafffffe87b93f035ad76
Jesus Gómez <jesus_jgb_45@hotmail.com>
2018-04-02 17:47:45 +0200

    create Home page

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import React, { Component } from 'react';
-import { Title } from './components/Title';
-import { SearchForm } from './components/SearchForm';
-import { MoviesList } from './components/MoviesList';
+
 import { Detail } from './pages/Detail';
+import { Home } from './pages/Home';
 
 import 'bulma/css/bulma.css';
 import './App.css';
 
 class App extends Component {
-  state = { usedSearch: false, results: [] };
-
-  _handleResults = results => {
-    this.setState({ results, usedSearch: true });
-  };
-
-  _renderResults = () => {
-    return this.state.results.length === 0 ? (
-      <p>Sorry! Results no found!</p>
-    ) : (
-      <MoviesList movies={this.state.results} />
-    );
-  };
-
   render() {
     const url = new URL(document.location);
-    const hasId = url.searchParams.has('id');
-    if (hasId) {
-      return <Detail id={url.searchParams.get('id')}/>;
-    }
-    return (
-      <div className="App">
-        <Title>Search Movie</Title>
-        <div className="search-form-wrapper">
-          <SearchForm onChange={this._handleResults} />
-        </div>
-        {this.state.usedSearch ? (
-          this._renderResults()
-        ) : (
-          <small>Use the form to search a movie</small>
-        )}
-      </div>
+    const Page = url.searchParams.has('id') ? (
+      <Detail id={url.searchParams.get('id')} />
+    ) : (
+      <Home />
     );
+
+    return <div className="App">{Page}</div>;
   }
 }
 
